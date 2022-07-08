57678e4e49a6e20503bbdaf8b3ef1244812ad652
tama <tama1234>
2022-07-08 19:09:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, { useState } from "react";
 import { DraggableArea } from 'react-draggable-tags';
 
-// import "rsuite/dist/rsuite.min.css";
-import { IconButton } from 'rsuite';
+import "rsuite/dist/rsuite.min.css";
+import { AutoComplete, IconButton } from 'rsuite';
 import { Plus } from '@rsuite/icons';
 
 const App = () => {
 
-  const renderButton = () => {
-    return (
-      <IconButton className="tag" id={111} icon={<Plus id={4555} />} />
-    )
-  };
-
-  const initialTags = [
+  const [tags, setTags] = useState([
     { id: 1, content: 'apple' }, { id: 2, content: 'olive' }, { id: 3, content: 'banana' },
-    { id: 4, content: 'lemon' }, { id: 5, content: 'orange' }
-  ];
+    { id: 4, content: 'lemon' }, { id: 5, content: 'orange' }, { id: 6, content: 'add' }
+  ]);
+
+  const [addTag, setAddTag] = useState(false);
+
+  const clickAddTag = () => {
+    setAddTag(true)
+  }
+
+  const handleAddTag = (v) => {
+    if (v.target.value.length) {
+      const _id = tags.length + 1;
+      tags.push({ id: _id, content: v.target.value });
+      fixTags(tags);
+    }
+    setAddTag(false)
+  }
+
+  const fixTags = (t) => {
+    var tmp = t.filter(v => v.content !== 'add')
+    setTags([...tmp, { id: 6, content: 'add' }])
+  }
+
   return (
     <div>
       <DraggableArea
-        tags={initialTags}
+        tags={tags}
+        isList={false}
         render={({ tag, index }) => {
-          if (tag.content) {
-            return <div id={index} className="tag">
+          if (tag.content !== 'add') {
+            return <div key={index.toString()} className="tag">
               {tag.content}
             </div>
           }
+          return addTag ? <AutoComplete autoFocus={true} onPressEnter={handleAddTag} /> : <IconButton onClick={clickAddTag} className="tag" key={index.toString()} icon={<Plus />} />;
         }}
-        onChange={tags => console.log((tags))}
+        onChange={_tags => {
+          fixTags(_tags)
+        }}
       />
-      {renderButton()}
     </div>
   );
 };
